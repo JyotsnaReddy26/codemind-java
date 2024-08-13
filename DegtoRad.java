9c2758bb789e4f2b3f5d045caea338f77f4eab18
JyotsnaReddy26 <147791987+JyotsnaReddy26@users.noreply.github.com>
2024-08-13 06:18:43 +0530

    Add files via upload

---
+++ DegtoRad.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class DegtoRad
+{
+    public static void main(String args[])
+{
+      Scanner sc=new Scanner(System.in);
+         Double degree,radians;
+           degree=sc.nextDouble();
+           radians=degree*(3.14/180);
+      System.out.println("Degrees in Radiansis:"+radians);
+  // System.out.printf("Degree in Radians is: %.2f",radians);
+              
+           
+             
+}
+}
+
